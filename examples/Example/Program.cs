6a9bfa1556bcb75168bbc0d560e7c88f19b6d199
Kristoffer Ahl <git@77dynamite.com>
2018-09-07 20:52:43 +0200

    - Added additional sample conventions.

--- examples/Example/Program.cs
+++ examples/Example/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using StructuredLogLines;
 
 namespace Example
@@ -13,12 +14,19 @@ namespace Example
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddLogging(configure =>
             {
+                // Default config
+                // configure.AddStructuredLogLines();
+
+                // Configuration overrides
                 configure.AddStructuredLogLines(sll =>
                 {
                     sll.MinimumLogLevel = LogLevel.Trace;
                     sll.UseColor = true;
-                    sll.UsePretty = false;
-                    //sll.With<CompatcLoggingConvetions>();
+                    sll.UsePretty = true;
+
+                    //sll.With<CompactLoggingConvetions>();
+                    //sll.With<EnrichedLoggingConvetions>();
+                    //sll.With<DotnetLoggingConventions>();
                 });
             });
 
@@ -76,7 +84,7 @@ namespace Example
         public string Name { get; set; }
     }
 
-    class CompatcLoggingConvetions : DefaultLoggerConventions
+    class CompactLoggingConvetions : DefaultLoggerConventions
     {
         public override string MessagePropertyName { get; } = "@m";
         public override string TimestampPropertyName { get; } = "@t";
@@ -84,5 +92,42 @@ namespace Example
         public override string SevertiyPropertyName { get; } = null;
         public override string EventPropertyName { get; } = "@e";
         public override string ContextPropertyName { get; } = "@c";
+    }
+
+    class EnrichedLoggingConvetions : DefaultLoggerConventions
+    {
+        public override Action<StructuredLogLinesLogger> OnLoggerCreated { get; } = logger =>
+        {
+            logger.BeginScope(new Origin { Service = "example-app", Logger = logger.Name });
+        };
+
+        class Origin
+        {
+            public string Service { get; set; }
+
+            public string Logger { get; set; }
+        }
+    }
+
+    class DotnetLoggingConventions : DefaultLoggerConventions
+    {
+        public override JsonSerializerSettings JsonSerializerSettings { get; } = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore
+        };
+
+        public override Func<object, string> ContextPathResolver { get; } = ctx =>
+        {
+            var type = ctx.GetType();
+            return type.FullName.StartsWith("System.") ? null : type.Name;
+        };
+
+        public override Func<object, string> EventPathResolver { get; } = ctx =>
+        {
+            var type = ctx.GetType();
+            return (ctx is Exception == false && type.FullName.StartsWith("System.")) ? null : type.Name;
+        };
+
+        public override Func<object, object> EventDataResolver { get; } = evt => evt;
     }
 }
